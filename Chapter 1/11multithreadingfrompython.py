395d6a464127ca07c79babc915913bee841d75ad
melancholicbud <pavadim.99@gmail.com>
2024-10-21 08:57:16 +0300

    threading & multi-threading

---
+++ Chapter 1/11multithreadingfrompython.py
@@ -0,0 +1,19 @@
+from multiprocessing import Pool
+import time
+def countup(N):
+    n = 0
+    while n < N:
+        n += 1
+
+if __name__ == '__main__':
+    max_for_process = 30000000//2
+    process_pool = Pool(processes=2)
+    st_time = time.time()
+    first_process = process_pool.apply_async(countup,
+                                             [max_for_process])
+    second_process = process_pool.apply_async(countup,
+                                              [max_for_process])
+    process_pool.close()
+    process_pool.join()
+    end_time = time.time()
+    print(f'Time elapsed: {end_time-st_time}')
---
+++ Chapter 1/8globalinterpreterlock.py
@@ -0,0 +1,13 @@
+import time
+def countup(N):
+    n = 0
+    while n < N:
+        n += 1
+
+if __name__ == '__main__':
+    st_time = time.time()
+    countup(30000000)
+    end_time = time.time()
+
+    print(f'Time elapsed: {end_time-st_time}')
+    
