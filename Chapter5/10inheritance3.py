56dd2df8d8755f905523b28e9c7cc6ac59416c42
melancholicbud <pavadim.99@gmail.com>
2024-10-30 08:08:08 +0200

    inheritance, pt. 2

---
+++ Chapter5/10inheritance3.py
@@ -0,0 +1,40 @@
+class BaseTextTest:
+    name = "Test"
+
+    def set_text(self):
+        self.text = "Class BaseTextTest"
+    
+    def print_base(self): 
+        return self.text
+
+    def __str__(self):
+        return self.text
+
+class NewTextTest(BaseTextTest):
+    def set_new_text(self):
+        self.text = "Class NewTextTest"
+    
+    def print_new(self):
+        return self.text
+    
+    def __str__(self):
+        return self.text
+    
+if __name__ == "__main__":
+    new_class = NewTextTest()
+    new_class.set_text()
+    print(new_class)
+    print(new_class.print_base())
+    print(new_class.print_new())
+    new_class.set_new_text()
+    print(new_class.print_new())
+    print(new_class)
+
+print(f'new_class = {new_class.name}',
+      f' NewTextTest = {NewTextTest.name}',
+      f'BaseTextTest = {BaseTextTest.name}')
+
+NewTextTest.name = "Not bad"
+print(f'new_class = {new_class.name}',
+      f' NewTextTest = {NewTextTest.name}',
+      f'BaseTextTest = {BaseTextTest.name}')
--- Chapter5/9inheritance2.py
+++ Chapter5/9inheritance2.py
@@ -17,3 +17,20 @@ class Square(Shape):
 
     def __str__(self):
         return super().__str__() + f', side: {self.side}'
+class Circle(Shape):
+    def __init__(self, radius=1, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+    
+    def __str__(self):
+        return super().__str__() + f', side: {self.radius}'
+
+if __name__ == "__main__":
+    my_circle = Circle(x=10, y=5)
+    my_square = Square(5, 25, 40)
+    print(my_circle)
+    print(my_square)
+    my_square.move(35, 50)
+    my_circle.move(10, 10)
+    print(my_circle)
+    print(my_square)
