a7e798d98428eeb62626594ddc66a0df34ede5e2
melancholicbud <pavadim.99@gmail.com>
2024-11-12 10:37:59 +0200

    some requesting

---
+++ Chapter10/requestwithnameparameters.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+if __name__ == "__main__":
+    db_name = 'my_todolist.db'
+    with sqlite3.connect(db_name) as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        # telling DB which data might be chose and sort by deadline order
+        query = """select id, priority, description, status,
+                    deadline from my_task where project = :my_proj_name
+                    order by deadline, priority"""
+        cursor.execute(query, {'my_proj_name': 'MagicMonth'})
+        # execute a data
+        for it_row in cursor.fetchall():
+            print(f"{it_row['id']}, "
+                  f"{it_row['priority']}, "
+                  f"{it_row['description']}, "
+                  f"{it_row['status']}, "
+                  f"{it_row['deadline']}, ")
